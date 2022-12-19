f71583ea135c41bf4b466fcb11f65ba00e2737a8
nathanpaul423 <nathanpaulbustamante123@gmail.com>
2022-12-19 16:32:30 +0800

    update python script

--- data_elevation/Script-python.py
+++ data_elevation/Script-python.py
@@ -2,21 +2,27 @@ import os
 import win32com.client
 
 text_file = "C:\\Users\\Tokwa\\Documents\\GitHub\\Project-1---Magat-Reservoir-Elevation-prediction\\data_elevation\\script.txt"
-
+parent_dir = r"C:\Users\Tokwa\Documents\GitHub\Project-1---Magat-Reservoir-Elevation-prediction"
 # Open the workbook
 xl = win32com.client.Dispatch("Excel.Application")
 # xl.Visible = True  # optional
-wb = xl.Workbooks.Open(os.path.abspath(r"C:\Users\Tokwa\Documents\GitHub\Project-1---Magat-Reservoir-Elevation-prediction\data_elevation\VBA - Script.xlsm"))
 
-# Add a new module
-module = wb.VBProject.VBComponents.Add(1)  # 1 stands for a module
+list = os.listdir(parent_dir)
+list_workingFolder = os.path.join(parent_dir,list[6])
+print(os.listdir(list_workingFolder))
+
+
+# wb = xl.Workbooks.Open(os.path.abspath(r"C:\Users\Tokwa\Documents\GitHub\Project-1---Magat-Reservoir-Elevation-prediction\data_elevation\VBA - Script.xlsm"))
 #
-module.CodeModule.AddFromFile(text_file)
-
-xl.Application.Run("Magat")
-
-# Save the workbook
-wb.Save()
-
-# Close the workbook
-xl.Quit()
+# # Add a new module
+# module = wb.VBProject.VBComponents.Add(1)  # 1 stands for a module
+# #
+# module.CodeModule.AddFromFile(text_file)
+#
+# xl.Application.Run("Magat")
+#
+# # Save the workbook
+# wb.Save()
+#
+# # Close the workbook
+# xl.Quit()
