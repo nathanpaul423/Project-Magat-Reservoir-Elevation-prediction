4460c6a1494da288062a4244245af4936e68d9bc
nathanpaul423 <nathanpaulbustamante123@gmail.com>
2022-12-20 16:27:43 +0800

    update Script
    update
    -dry run code if it works

--- data_elevation/Script-python.py
+++ data_elevation/Script-python.py
@@ -1,17 +1,31 @@
 import os
 import win32com.client
+import openpyxl
 
+#directories
 text_file = "C:\\Users\\Tokwa\\Documents\\GitHub\\Project-1---Magat-Reservoir-Elevation-prediction\\data_elevation\\script.txt"
-parent_dir = r"C:\Users\Tokwa\Documents\GitHub\Project-1---Magat-Reservoir-Elevation-prediction"
+parent_dir = r"C:\Users\Tokwa\Documents\GitHub\Project-1---Magat-Reservoir-Elevation-prediction\\workingFolder"
+
+
+list = os.listdir(parent_dir)
+file_list=[]
+for i in list:
+    file_list.append(os.path.join(parent_dir,i))
+
+list_xlsm=[]
+for i in list:
+    list_xlsm.append(i.replace('.xlsx','.xlsm'))
+
+#converts .xlsx to .xlsm
+for i,j in zip(file_list,list_xlsm):
+    wb = openpyxl.load_workbook(i)
+    wb.save(os.path.join(parent_dir,j))
+
+
+# write VBA script.
 # Open the workbook
 xl = win32com.client.Dispatch("Excel.Application")
 # xl.Visible = True  # optional
-
-list = os.listdir(parent_dir)
-list_workingFolder = os.path.join(parent_dir,list[6])
-print(os.listdir(list_workingFolder))
-
-
 # wb = xl.Workbooks.Open(os.path.abspath(r"C:\Users\Tokwa\Documents\GitHub\Project-1---Magat-Reservoir-Elevation-prediction\data_elevation\VBA - Script.xlsm"))
 #
 # # Add a new module
